ee4f15452121045dd586520d10a3ee2a96c5cedd
LikeCiastka <98387159+LikeCiastka@users.noreply.github.com>
2022-01-25 11:25:59 +0100

    Update odejmowanie.h

--- odejmowanie.h
+++ odejmowanie.h
@@ -1,4 +1,4 @@
 void odejmowanie(int liczba1, int liczba2){
     int wynik=liczba1-liczba2;                                             //odejmuje hehe
-   printf("Wynik odejmowania tych dwoch liczb wynosi: %d", &wynik);
+             printf("Wynik odejmowania tych dwoch liczb wynosi: %d", &wynik); //wynik hehe
 }
