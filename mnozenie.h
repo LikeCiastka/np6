b13bb9bd0628957cb81b04de3a1c82415fd3ac4f
Supomisve <98385591+Supomisve@users.noreply.github.com>
2022-01-25 10:53:58 +0100

    Update mnozenie.h

--- mnozenie.h
+++ mnozenie.h
@@ -1,4 +1,4 @@
 void mnozenie(int liczba1, int liczba2){
-    int wynik=liczba1 * liczba2;
+    int wynik=liczba1 * liczba2;                                             //mnozenie hehe
     printf("Wynik mnozenia tych dwoch liczb wynosi: %d", &wynik);
 }
