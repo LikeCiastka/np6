d65511a6e46c4751800f6d9355e0a68f38f669d5
LikeCiastka <98387159+LikeCiastka@users.noreply.github.com>
2022-01-25 12:38:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
     int liczba1, liczba2
     printf("Wybierz operacje (1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie");
         scanf("%c", &znak)
-    printf("Podaj dwie liczby")
+    printf("Podaj dwie liczby") ///użytkownik wprowadza 2 liczby
         scanf("%d", &liczba1)
     scanf("%d", &liczba2)
 }
