1be988bb054a7308096fba6a3d8623684c738867
Supomisve <98385591+Supomisve@users.noreply.github.com>
2022-01-25 11:09:32 +0100

    Update dodawanie.h

--- dodawanie.h
+++ dodawanie.h
@@ -1,4 +1,4 @@
 void dodawanie(int liczba1, int liczba2){
-    int wynik=liczba1+liczba2;
-    printf("Wynik dodawania tych dwoch liczb wynosi: %d", &wynik);
+    int wynik=liczba1+liczba2;                                              //dodawanie
+    printf("Wynik dodawania tych dwoch liczb tego typu wynosi: %d", &wynik);
 }
