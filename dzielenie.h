715a4a937d4ed8c784ca9c88d9aeb6d45ef4ed40
LikeCiastka <98387159+LikeCiastka@users.noreply.github.com>
2022-01-25 12:43:30 +0100

    Update dzielenie.h

--- dzielenie.h
+++ dzielenie.h
@@ -1,4 +1,4 @@
 void dzielenie(int liczba1, int liczba2){
     int wynik=liczba1/liczba2;                                           //dzieli es
-    printf("Wynik dzielenia tych dwoch liczb tego typu hxddd wynosi: %d", &wynik); //wypisuje wynik
+    printf("Wynik dzielenia tych dwoch liczb tego typu hxddd wynosi: %d", &wynik); //printuje wynik dzielenia
 }
