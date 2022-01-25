f1e619ad81334b344108e01182d64d0c342a1241
LikeCiastka <98387159+LikeCiastka@users.noreply.github.com>
2022-01-25 11:24:46 +0100

    Update mnozenie.h

--- mnozenie.h
+++ mnozenie.h
@@ -1,4 +1,4 @@
 void mnozenie(int liczba1, int liczba2){
-    int wynik=liczba1 * liczba2;                                             //mnozenie heheasas
+            int wynik=liczba1 * liczba2;                                             //mnozenie heheasas
     printf("Wynik mnozenia tych dwoch liczb tego typu wynosi: %d", &wynik);
 }
