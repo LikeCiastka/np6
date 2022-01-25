5be183661e3acb01e49db564e8d5aa5dd6be4a0c
Kampojet <98385866+Kampojet@users.noreply.github.com>
2022-01-25 11:18:23 +0100

    Update mnozenie.h

--- mnozenie.h
+++ mnozenie.h
@@ -1,4 +1,3 @@
 void mnozenie(int liczba1, int liczba2){
-    int wynik=liczba1 * liczba2;                                             //mnozenie hehe
-    printf("Wynik mnozenia tych dwoch liczb wynosi: %d", &wynik);
+    int wynik=liczba1 * liczba2;                                             //mnozenie hehenik);
 }
