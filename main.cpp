3bd4038cc880677c985b76dbe51ae4e48bf0b55b
Kampojet <98385866+Kampojet@users.noreply.github.com>
2022-01-25 10:33:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,24 @@
 #include "dzielenie.h"
 
 int main(){
-    char znak;
-    int liczba1, liczba2
+    int znak, liczba1, liczba2;
     printf("Wybierz operacje (1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie");
-    scanf("%c", &znak)
-    printf("Podaj dwie liczby")
-    scanf("%d", &liczba1)
-    scanf("%d", &liczba2)
+    scanf("%d", &znak);
+    printf("Podaj dwie liczby");
+    scanf("%d", &liczba1);
+    scanf("%d", &liczba2);
+    switch (znak){
+        case 1;
+            dodawanie();
+            break;
+        case 2;
+            odejmowanie();
+            break;
+        case 3;
+            mnozenie();
+            break
+        case 4;
+            dzielenie();
+            break;
+    }
 }
